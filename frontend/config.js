6b9eac52cd96bf7830f85762848e493c9f49d81a
麻糬 <liu.ethan0720@yahoo.com>
2025-12-05 20:28:58 +0800

    beta0.4

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@ const API_CONFIG = {
     // 本地開發環境
     development: 'http://localhost:3000',
     // 生產環境 - 部署後改成你的 Render URL
-    production: 'https://ncu-courses-select.onrender.com'
+    production: 'https://ncu-courses-select.vercel.app'
 };
 
 // 自動偵測環境
